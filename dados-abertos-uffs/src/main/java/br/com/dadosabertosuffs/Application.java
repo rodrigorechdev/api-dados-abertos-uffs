c7764afc9fc9d5372928590571ab15fd0dd2b640
Rodrigo Rech <rodrigorech@hotmail.com>
2022-06-21 03:44:49 -0300

    base projeto

---
+++ dados-abertos-uffs/src/main/java/br/com/dadosabertosuffs/Application.java
@@ -0,0 +1,29 @@
+package br.com.dadosabertosuffs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
+
+@EnableSwagger2
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public Docket swagger() {
+        return new Docket(SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.any())
+            .build();
+    }
+
+}
