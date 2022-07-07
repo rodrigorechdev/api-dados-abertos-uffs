80c25ced240b0e16fd579d787f74b60b3201e595
Rodrigo Rech <rodrigorech@hotmail.com>
2022-07-07 02:11:48 -0300

    @AllArgsConstructor ColunasRelacionadas

--- dados-abertos-uffs/src/main/java/br/com/dadosabertosuffs/entity/dto/ColunasRelacionadas.java
+++ dados-abertos-uffs/src/main/java/br/com/dadosabertosuffs/entity/dto/ColunasRelacionadas.java
@@ -1,5 +1,6 @@
 package br.com.dadosabertosuffs.entity.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,6 +8,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
 public class ColunasRelacionadas {
     String nomeDataset;
     String nomeCampo;
